1c54a889d54a41ddab48c830880f9e6eabf243b4
Nashte13 <nashmwangi26@gmail.com>
2026-09-15 13:46:30 +0300

    update on base url

--- unicafe-tests/tests/unicae.spec.js
+++ unicafe-tests/tests/unicae.spec.js
@@ -1,7 +1,7 @@
 import {test, expect} from '@playwright/test';
 
 test('feedback buttons are visible', async ({ page }) => {
-    await page.goto('http://localhost:5173')
+    await page.goto('/')
     await expect(page.getByText('give feedback')).toBeVisible()
     await expect(page.getByRole('button', { name: 'good' })).toBeVisible()
     await expect(page.getByRole('botton', { name: 'neutral' })).toBeVisible()
@@ -9,19 +9,19 @@ test('feedback buttons are visible', async ({ page }) => {
 })
 
 test('statistics show "No feedback given" initially', async ({ page }) => {
-    await page.goto('http://localhost:5173')
+    await page.goto('/')
     await expect(page.getByText('No feedback given')).toBeVisible()
 })
 
 test('clicking good increases statistics', async ({ page }) => {
-    await page.goto('http://localhost:5173')
+    await page.goto('/')
     await page.getByRole('button', { name: 'good' }).click()
     await expect(page.getByText('good')).toBeVisible()
     await expect(page.getByText('1')).toBeVisible()
 })
 
 test('clicking neutral and bad updates statistics', async ({ page }) => {
-    await page.goto('http://localhost:5173')
+    await page.goto('/')
     await page.getByRole('button', { name: 'neutral' }).click()
     await page.getByRole('button', { name: 'bad' }).click()
     await expect(page.getByText('neutral')).toBeVisible()
