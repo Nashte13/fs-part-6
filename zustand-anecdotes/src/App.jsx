b6b29aaeb11dd84f8d94d4a424a72a7dedf6e96b
Nashte13 <nashmwangi26@gmail.com>
2026-09-24 15:19:45 +0300

    making updates

--- zustand-anecdotes/src/App.jsx
+++ zustand-anecdotes/src/App.jsx
@@ -1,6 +1,9 @@
+import { useEffect } from 'react';
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter';
+import { useAnecdoteStore } from './store';
+import anecdoteService from './services/anecdotes'
 
 const App = () => {
 
