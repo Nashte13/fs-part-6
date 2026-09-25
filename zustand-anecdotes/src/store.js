a354acb11c5a1c6586c7f983bf1854803191f819
Nashte13 <nashmwangi26@gmail.com>
2026-09-25 14:34:41 +0300

    voting function update

--- zustand-anecdotes/src/store.js
+++ zustand-anecdotes/src/store.js
@@ -6,12 +6,41 @@ export const useAnecdoteStore = create((set) => ({
   filter: "",
   actions: {
     setAnecdotes: (anecdotes) => set({ anecdotes }),
-    vote: (id) =>
+    vote: async (id) => {
+      set((state) => {
+        const anecdote = state.anecdotes.fid((a) => String(a.id) === String(id))
+
+        if (!anecdote) {
+          return state
+        }
+
+        const updatedAnecdote = {
+          ...anecdote,
+          votes: anecdote.votes + 1
+        }
+
+        return {
+          anecdotes: state.anecdotes.map((a) =>
+            String(a.id) === String(id) ? updatedAnecdote : a
+          ),
+        }
+      })
+
+      const currentState = useAnecdoteStore.getState()
+      const anecdote = currentState.anecdotes.find((a) => String(a.id) === String(id))
+
+      if (!anecdote) return
+      const updatedAnecdote = await anecdoteService.updateVote(
+        id,
+        anecdote.votes + 1
+      )
+
       set((state) => ({
         anecdotes: state.anecdotes.map((a) =>
-          a.id === id ? { ...a, votes: a.votes + 1 } : a,
+          String(a.id) === String(updatedAnecdote.id) ? updatedAnecdote : a
         ),
-      })),
+      }))
+    },
     add: async (content) => {
       const newAnecdote = await anecdoteService.createNew(content);
 
